b312e2776c5093b90f090d0df8f04fb3c25701b0
Erick08-web <123047832@upq.edu.mx>
2026-06-09 22:25:36 -0600

    Practica 6 - Componentes nativos y propios

--- Practica 5/miapp2/App.js
+++ Practica 5/miapp2/App.js
@@ -1,15 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* Zona 1 : Importaciones de Componentes y archivos*/
 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import { Saludo} from './components/Saludo';
+import { Saludo2} from './components/Saludo2';
+import { Perfil } from './components/Perfil';
+
+
+
+/* Zona 2: Main - Hogar de los componentes */
 export default function App() {
   return (
     <View style={styles.container}>
+
+      
+      <Image source={require('./assets/wave.png')}/> 
+
+      <Text>----------------- Componentes Nativos--------------</Text>
+
       <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>----------------- Componentes Propio Simple--------------</Text>
+
+      <Saludo></Saludo>
+
+      <Text>----------------- Componentes Propio Compuesto --------------</Text>
+
+      <Saludo2></Saludo2>
+
+      <Text>----------------- Perfil--------------</Text>
+
+      <Perfil></Perfil>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/* Zona 3: Zona de los estilos y posicionamientos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ Practica 5/miapp2/components/Perfil.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export const Perfil = () => {
+    return(
+        <View>
+            <Text>Nombre: Erick Alvarez Balderas</Text>
+            <Text>Carrera: Ingeniería en Sistemas</Text>
+            <Text>Materia: Programación Movil</Text>
+            <Text>Cuatrimestre: Noveno</Text>
+        </View>
+    )
+}
