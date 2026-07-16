c50cf44067680e5f24ea0ad6a641f62021f352bd
Erick08-web <123047832@upq.edu.mx>
2026-07-15 23:47:01 -0600

    Add usuarioApi Expo app

---
+++ usuarioApi/App.js
@@ -0,0 +1,16 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import AltaUsuariosScreen from './screens/AltaUsuariosScreen';
+import ConsultaUsuariosScreen from './screens/ConsultaUsuariosScreen';
+
+
+
+export default function App() {
+
+  return (
+
+    <AltaUsuariosScreen></AltaUsuariosScreen>
+
+  );
+
+}
+
